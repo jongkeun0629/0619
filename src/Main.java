7d3586a6c6fe3b39032bc363ab0f77cc46670dc0
jongkeun0629 <dlwhdrms0629@gmail.com>
2025-06-19 14:42:57 +0900

    printElements

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,19 @@
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-class Box{
-    public static <T> void displayArray(List<T> array){
-        for(T element: array)
-            System.out.println(element);
-    }
-}
-
 public class Main {
+    // 리스트 내용 출력만 하는 구조. 어떤 형태로 동작하든 상관없음
+    public static void printElements(List<?> list){
+        for(Object o : list){
+            System.out.println(o);
+        }
+    }
+
     public static void main(String[] args) {
-        List<Integer> numbers = new ArrayList<>(List.of(1, 2, 3, 4, 5));
+        List<Integer> numbers = Arrays.asList(1, 2, 3);
+        List<String> words = Arrays.asList("A", "B", "CC");
 
-        numbers.add(6);
-        numbers.remove(4);
-
-        List<String> words = new ArrayList<>(Arrays.asList("apple", "banana", "cherry"));
-
-        Box.displayArray(numbers);
-        Box.displayArray(words);
-
-
+        printElements(numbers);
+        printElements(words);
     }
 }
